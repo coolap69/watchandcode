59baa45b2d0cda9b290829c5028abb23792a81db
Louis Pazos <louispazos@gmail.com>
2017-12-31 01:26:31 +0000

    types and comparison

---
+++ dataTypesComparison/dataTypesComparison.js
@@ -0,0 +1,30 @@
+//comparison with primitives//
+100 === 100
+true
+
+'louis' === 'louis'
+true
+
+'louis1' === 'louis'
+false
+
+null === null
+true
+
+2 === 3
+false
+
+// comparison with objects//
+
+//different location= false//
+{} === {}
+false
+
+[1, 2, 3] === [1, 2, 3]
+false
+
+var houseA = {};
+undefined
+
+houseA === houseA
+true
